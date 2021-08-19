fe85fd78548909d34205ee84404a339f6d228027
huequica <dev@huequica.xyz>
2021-08-19 20:44:53 +0900

    🚚 common -> singleFunctions

--- tests/singleFunctions.test.ts
+++ tests/singleFunctions.test.ts
@@ -1,10 +1,12 @@
-import * as Commons from '~/extensions/singleFunctions';
+import * as Commons from '~/index';
 
 const testCase = ['hoge', 'fuga', undefined, 'piyo', undefined];
 
 describe('🧐 Array#lastItemIs', (): void => {
 	test('🙆‍♀️ Filtering undefined value', (): void => {
-		const filtered = testCase.filter((target) => !Commons.isUndefined(target));
+		const filtered = testCase.filter(
+			(target) => !Commons.singleFunctions.isUndefined(target)
+		);
 		expect(filtered).toEqual(['hoge', 'fuga', 'piyo']);
 	});
 });
